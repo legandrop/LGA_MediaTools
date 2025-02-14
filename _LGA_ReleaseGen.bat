6535ed0181a5ec2e40d4aaa5488d42dbc88251ad
legandrop <176236735+legandrop@users.noreply.github.com>
2025-02-14 09:52:23 -0300

    v1.4 - Nuevo EXR to PIZ + Mejorado el ReleaseGen

--- _LGA_ReleaseGen.bat
+++ _LGA_ReleaseGen.bat
@@ -1,14 +1,61 @@
 @echo off
-rem Change directory to the desired folder
-cd /d "C:\Portable\LGA_MediaTools"
+setlocal enabledelayedexpansion
 
-rem Check if the .zip file exists and delete it if it does
-if exist LGA_MediaTools.zip (
-    del LGA_MediaTools.zip
+rem Cambiar directorio a la ubicación del script
+cd /d "%~dp0"
+
+rem Definir nombre base del ZIP
+set "ZIPBASE=LGA_MediaTools"
+set "VERSION_PATTERN=%ZIPBASE%_v*.zip"
+
+rem Inicializar variables de versión
+set "max_ver=0"
+set "version_exists=false"
+
+rem Buscar versión máxima existente
+for %%F in (%VERSION_PATTERN%) do (
+    set "version_exists=true"
+    set "filename=%%~nF"
+    set "ver_str=!filename:%ZIPBASE%_v=!"
+    
+    for /f "tokens=1 delims=_" %%G in ("!ver_str!") do (
+        set "current_ver=%%G"
+        set "int_ver=!current_ver:.=!"
+        
+        if !int_ver! GTR !max_ver! (
+            set "max_ver=!int_ver!"
+        )
+    )
 )
 
-rem Create the zip file with exclusions from the specified folder
-"C:\Program Files\7-Zip\7z.exe" a -tzip LGA_MediaTools.zip * -xr@.exclude.lst
+rem Calcular nueva versión
+if "!version_exists!"=="false" (
+    set "new_version=1.0"
+) else (
+    set /a new_ver_num=max_ver + 1
+    set /a major=new_ver_num / 10
+    set /a minor=new_ver_num %% 10
+    set "new_version=!major!.!minor!"
+)
 
-rem Pause the script to see any error messages
+rem Crear nombre del archivo final
+set "FINAL_ZIP=%ZIPBASE%_v!new_version!.zip"
+
+rem Eliminar ZIP anterior si existe
+if exist "!FINAL_ZIP!" (
+    del "!FINAL_ZIP!"
+)
+
+rem Empaquetar con 7-Zip
+echo Creando paquete !FINAL_ZIP!...
+"C:\Program Files\7-Zip\7z.exe" a -tzip "!FINAL_ZIP!" * -xr@.exclude.lst
+
+rem Resultado final
+echo.
+echo ========================================
+echo Paquete generado: !FINAL_ZIP!
+echo ========================================
+echo.
+
 pause
+endlocal
